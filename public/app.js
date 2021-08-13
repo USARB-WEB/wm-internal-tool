1f772aa83eed73c8fbdd1f3b066668fc479083d0
sergiuchilat <chilatsergiu@gmail.com>
2021-08-13 22:46:39 +0300

    Change API URL

--- public/app.js
+++ public/app.js
@@ -1,17 +1,18 @@
 var app = new Vue({
     el: '#app',
     data: {
-        selectedCase: 1,
+        selectedCase: 0,
         totalCases: 13,
         scale: {
             lines: 20,
             lineHeight: 25
         },
-        apiURL: "http://127.0.0.1:5500/db/",
+        //apiURL: "http://127.0.0.1:5500/db/",
+        apiURL: "https://raw.githubusercontent.com/sergiuchilat/wm-internal-tool/master/db/",
         ranges: {}
     },
     mounted(){
-        this.selectCase(7);
+        this.selectCase(5);
     },
     methods: {
         calculateOffset(start){
