4abafd708f2b3af80e7321f3bc540fbf0730d744
Sergiu <chilatsergiu@gmail.com>
2021-08-16 11:39:06 +0300

    v2.0 algorithm

--- public/app.js
+++ public/app.js
@@ -7,12 +7,19 @@ var app = new Vue({
             lines: 20,
             lineHeight: 25
         },
-        // apiURL: "http://127.0.0.1:5500/db/",
-        apiURL: "https://raw.githubusercontent.com/sergiuchilat/wm-internal-tool/master/db/",
+        apiURL: "http://127.0.0.1:5500/db/",
+        // apiURL: "https://raw.githubusercontent.com/sergiuchilat/wm-internal-tool/master/db/",
         ranges: {}
     },
+    computed:{
+        resultingRanges() {
+            return this.ranges.results?.map(range => {
+                return this.ranges.existing[range.new.index]?.selected ? range.existing : range.new;
+            });
+        }
+    },
     mounted(){
-        this.selectCase(1);
+        this.selectCase(2);
     },
     methods: {
         calculateOffset(start){
@@ -20,65 +27,6 @@ var app = new Vue({
         },
         calculateHeight(start, end) {
             return this.scale.lineHeight * ( end -  start);
-        },
-        generateResults(){
-            const results = [];
-            for(const newRange of this.ranges.new) {
-                for(const existingRange of this.ranges.existing) {                      
-                    if(existingRange.start <= newRange.start && existingRange.end >= newRange.end){
-                        return [
-                            {
-                                "companyName": "ERROR",
-                                "companyAddress": "this combination can't be saved",
-                                "startIP": "",
-                                "endIP": "",
-                                "start": newRange.start,
-                                "end": newRange.end,
-                                type: 'error'
-                            }
-                        ]
-                    } else if(existingRange.start === newRange.start && existingRange.end < newRange.end){
-                        existingRange.type = 'existing';
-                        results.push(existingRange);
-                        newRange.start = existingRange.end;
-                        newRange.type = 'new';
-                        results.push(newRange);
-                    } else if(existingRange.end === newRange.end && existingRange.start > newRange.start){
-                        existingRange.type = 'existing';
-                        results.push(existingRange);
-                        newRange.end = existingRange.start;
-                        newRange.type = 'new';
-                        results.push(newRange);
-                    } else if(existingRange.start > newRange.start && existingRange.end < newRange.end){
-                        const newRangeFirst = Object.assign({}, newRange);
-                        const newRangeSecond = Object.assign({}, newRange);
-
-                        newRangeFirst.end = existingRange.start;
-                        newRangeSecond.start = existingRange.end;
-
-                        newRangeFirst.type = 'new';
-                        existingRange.type = 'existing';
-                        newRangeSecond.type = 'new';
-
-                        results.push(newRangeFirst);
-                        results.push(existingRange);
-                        results.push(newRangeSecond);
-                    } else if (existingRange.start < newRange.start && existingRange.end > newRange.start){
-                        existingRange.type = 'existing';
-                        results.push(existingRange);
-                        newRange.start = existingRange.end;
-                        newRange.type = 'new';
-                        results.push(newRange);
-                    } else if (existingRange.start < newRange.end && existingRange.end > newRange.end){
-                        existingRange.type = 'existing';
-                        results.push(existingRange);
-                        newRange.end = existingRange.start;
-                        newRange.type = 'new';
-                        results.push(newRange);
-                    }
-                } 
-            }
-            return results;
         },
         async selectCase(caseID){
             if(!caseID){
@@ -104,14 +52,29 @@ var app = new Vue({
                         height: this.calculateHeight(el.start, el.end)
                     }
                 });
-                this.ranges.results = this.generateResults().map(el => {
+                this.ranges.results = this.ranges.results?.map(el => {
+                    let existingRange = null;
+                    let newRange = null;
+                    if(el.existing) {
+                        existingRange = Object.assign({}, {
+                            ...el.existing,
+                            startOffset: this.calculateOffset(el.existing.start),
+                            height: this.calculateHeight(el.existing.start, el.existing.end)
+                        });
+                    }
+                    if(el.new) {
+                        newRange = Object.assign({}, {
+                            ...el.new,
+                            startOffset: this.calculateOffset(el.new.start),
+                            height: this.calculateHeight(el.new.start, el.new.end)
+                        });
+                    }
                     return {
-                        ...el,
-                        startOffset: this.calculateOffset(el.start),
-                        height: this.calculateHeight(el.start, el.end)
+                        existing: existingRange,
+                        new: newRange
                     }
                 });
-                
+                console.log(this.ranges.results);
             }catch(e){
                 console.log(e);
             }
